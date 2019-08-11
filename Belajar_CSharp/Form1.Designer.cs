9ab91cd160645d0e44068ae4aba18f7159ffd4b7
HP <HP@DESKTOP-SE0CSL8>
2019-08-11 13:47:30 +0700

    Initial Commit

--- Belajar_CSharp/Form1.Designer.cs
+++ Belajar_CSharp/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(56, 85);
+            this.button1.Location = new System.Drawing.Point(96, 139);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 45);
             this.button1.TabIndex = 0;
