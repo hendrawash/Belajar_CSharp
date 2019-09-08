93c81f7a09ed140e8c90afb994299464963996fb
HP <HP@DESKTOP-SE0CSL8>
2019-09-08 15:00:23 +0700

    Loop For

---
+++ Loop_For/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Berapa kali mau melakukan perulangan : ");
+
+            int perulangan = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= perulangan; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Loop_For_Break/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_For_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Di angka berapa perulangan ingin keluar? : ");
+
+            int keluar = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.WriteLine(i);
+
+                if(i==keluar) break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
