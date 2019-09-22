f4c5092caf388211844b530a5eb98ee5d67ca65a
HP <HP@DESKTOP-SE0CSL8>
2019-09-22 09:47:26 +0700

    While-dowhile

---
+++ Loop_do_while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loop_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char diterima = 'Y';
+
+            do
+            {
+                Console.Write("You will be my GF ? (Y/T)");
+                diterima = Convert.ToChar(Console.ReadLine());
+            } while (diterima != 'Y');
+
+            Console.WriteLine("Terima kasih sudah menerimaku");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Loop_while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loop_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char diterima ='T';
+
+            while (diterima!='Y')
+            {
+                Console.Write("You will be my GF ? (Y/T)");
+                diterima = Convert.ToChar(Console.ReadLine());
+            }
+
+            Console.WriteLine("Terima kasih sudah menerimaku");
+            Console.ReadLine();
+        }
+    }
+}
